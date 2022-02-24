a095990ea6156256dd65769caeea258675362e84
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-02-24 12:20:21 +0000

    Complete Exercise 6.2 - 6.5

--- Exercises/Chapter6Exercises.cpp
+++ Exercises/Chapter6Exercises.cpp
@@ -200,39 +200,268 @@ int Chapter6Exercises::Exercise6_4() {
 void Chapter6Exercises::L6_4_printCurrentDateTime() {
     // Finds the current date, and time, and prints this to the user.
 
-    L6_4_printCurrentTime();
-    L6_4_printCurrentDate();
+    unsigned int totalSeconds, totalMinutes, totalHours;
+    totalSeconds = time(0);
+    totalMinutes = totalSeconds / 60;
+    totalHours = totalMinutes / 60;
+
+    L6_4_printCurrentTime(totalSeconds, totalMinutes, totalHours);
+    L6_4_printCurrentDate(totalSeconds, totalHours);
 
 }
-void Chapter6Exercises::L6_4_printCurrentTime() {
+void Chapter6Exercises::L6_4_printCurrentTime(const unsigned int &totalSeconds,
+                                              const unsigned int &totalMinutes,
+                                              const unsigned int &totalHours) {
     // Finds and prints the current time
 
-    unsigned int totalSeconds, totalMinutes, totalHours;
-    unsigned short currentSeconds, currentMinutes, currentHours;
-
     // Finds the total elapsed seconds since the UNIX epoch [time(0)], and convert that to minutes and hours
-    totalSeconds    =   time(0);
-    totalMinutes    =   totalSeconds / 60;
-    totalHours      =   totalMinutes / 60;
+
 
     // Find the current time
-    currentSeconds   =   totalSeconds % 60;
-    currentMinutes   =   totalMinutes % 60;
-    currentHours    =   totalHours % 24;
-
-    // Display current time to user in HH:MM:SS format
-    std::cout   << currentHours << ":" << currentMinutes << ":" << currentSeconds << "." << std::endl;
+    L6_4_printHours(totalHours);
+    cout << ":";
+    L6_4_printMinutes(totalMinutes);
+    cout << ":";
+    L6_4_printSeconds(totalSeconds);
+    cout << endl;
 }
-void Chapter6Exercises::L6_4_printCurrentDate() {
+void Chapter6Exercises::L6_4_printHours(unsigned int totalHours) {
+    // Prints the current number of seconds
+
+    unsigned short currentHours = totalHours % 24;
+
+    if (currentHours < 10) {
+        cout << "0" << currentHours;
+    } else
+        cout << currentHours;
+}
+void Chapter6Exercises::L6_4_printMinutes(unsigned int totalMinutes) {
+    // Prints the current number of seconds
+
+    unsigned short currentMinutes = totalMinutes % 60;
+
+    if (currentMinutes < 10) {
+        cout << "0" << currentMinutes;
+    } else
+        cout << currentMinutes;
+}
+void Chapter6Exercises::L6_4_printSeconds(unsigned int totalSeconds) {
+    // Prints the current number of seconds
+
+    unsigned short currentSeconds = totalSeconds % 60;
+
+    if (currentSeconds < 10) {
+        cout << "0" << currentSeconds;
+    } else
+        cout << currentSeconds;
+}
+void Chapter6Exercises::L6_4_printCurrentDate(const unsigned int &totalSeconds,
+                                              const unsigned int &totalHours) {
+
     // Finds and prints the current date in DD/MM/YYYY format
 
-    L6_4_printYear();
+    unsigned int totalDays = totalHours / 24;
+    unsigned short currentYear = 1970, totalYearsSinceEpoch = 0; // UNIX Epoch is 1 Jan 1970
+    unsigned int currentDay = 0; // Which day today is
+    unsigned short numberOfLeapYears = 0; // How many leap years between Epoch and this year
+
+    L6_4_findYear(currentYear, totalDays, totalYearsSinceEpoch, numberOfLeapYears);
+    bool isCurrentYearLeapYear = (currentYear % 400 == 0 || (currentYear % 4 == 0 && currentYear % 100 != 0)) ? true : false;
+
+    // The +1 is an offset due to the first day of the year being 1st Jan, not 0th Jan, which is where code would begin
+    // to count
+    int totalDaysOfCurrentYear = totalDays - (totalYearsSinceEpoch * 365) - numberOfLeapYears + 1;
+
+    string currentMonth = L6_4_findCurrentMonth(totalDaysOfCurrentYear, currentDay, isCurrentYearLeapYear);
+
+    cout << currentDay << " " << currentMonth << " " << currentYear << endl;
 }
-void Chapter6Exercises::L6_4_printYear() {
+void Chapter6Exercises::L6_4_findYear(unsigned short &currentYear,
+                                      const unsigned int &totalDays,
+                                      unsigned short &totalYearsSinceEpoch,
+                                      unsigned short &numberOfLeapYears) {
+    // Increment multiple function-wide counters to find the current year, number of leap years, and how many
+    // years have passed since the UNIX Epoch
 
-    cout << "Enter the year";
-    int year;
-    cin >> year;
+    // The starting year is the UNIX Epoch: 1st Jan 1970
+    unsigned short startingYear = currentYear;
 
-    cout << "The year is: " << year;
+    // Be careful! currentYear, totalYearsSinceEpoch, and numberOfLeapYears, are references! This means each time they
+    // are changing in this function, they are changed for the rest of the program!
+
+    for (int tempdays = 0; tempdays <= totalDays; tempdays += 365) {
+        // Adds
+        currentYear++;
+
+        if (currentYear % 400 == 0 || (currentYear % 4 == 0 && currentYear % 100 != 0)) {
+            // If the current year is a leap year, add to counter
+            numberOfLeapYears++;
+            tempdays++;
+        }
+    }
+    currentYear--; // Counter oversteps, so this excludes endpoint
+    totalYearsSinceEpoch = currentYear - startingYear; // Exclude starting year itself from count
+}
+
+string Chapter6Exercises::L6_4_findCurrentMonth(unsigned int totalDaysOfCurrentYear, unsigned int &currentDay, const bool &isCurrentYearLeapYear) {
+    // Output the name of the current month as a string
+
+
+    // If current year is leap year, add extra day to February
+    unsigned short leapYearAddition = (isCurrentYearLeapYear ? 1 : 0);
+
+    // List of how many days in each month
+    unsigned short daysOfJan = 31,
+                   daysOfFeb = daysOfJan + 28 + leapYearAddition,
+                   daysOfMar = daysOfFeb + 31,
+                   daysOfApr = daysOfMar + 30,
+                   daysOfMay = daysOfApr + 31,
+                   daysOfJun = daysOfMay + 30,
+                   daysOfJul = daysOfJun + 31,
+                   daysOfAug = daysOfJul + 31,
+                   daysOfSep = daysOfAug + 30,
+                   daysOfOct = daysOfSep + 31,
+                   daysOfNov = daysOfOct + 30,
+                   daysOfDec = daysOfNov + 31;
+
+    // Range for totalDaysOfCurrentYear is [0, 364] (inclusive) in a non-leap year. This means that if the
+    // totalDaysOfCurrentYear < 31 then the date is January (and so on)
+    if (totalDaysOfCurrentYear < daysOfJan)
+    {
+        currentDay = totalDaysOfCurrentYear;
+        return "Jan";
+    }
+    else if (totalDaysOfCurrentYear < daysOfFeb)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfJan;
+        return "Feb";
+    }
+    else if (totalDaysOfCurrentYear < daysOfMar)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfFeb;
+        return "Mar";
+    }
+    else if (totalDaysOfCurrentYear < daysOfApr)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfMar;
+        return "Apr";
+    }
+    else if (totalDaysOfCurrentYear < daysOfMay)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfApr;
+        return "May";
+    }
+    else if (totalDaysOfCurrentYear < daysOfJun)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfMay;
+        return "Jun";
+    }
+    else if (totalDaysOfCurrentYear < daysOfJul)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfJun;
+        return "Jul";
+    }
+    else if (totalDaysOfCurrentYear < daysOfAug)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfJul;
+        return "Aug";
+    }
+    else if (totalDaysOfCurrentYear < daysOfSep)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfAug;
+        return "Sep";
+    }
+    else if (totalDaysOfCurrentYear < daysOfOct)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfSep;
+        return "Oct";
+    }
+    else if (totalDaysOfCurrentYear < daysOfNov)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfOct;
+        return "Nov";
+    }
+    else if (totalDaysOfCurrentYear < daysOfDec)
+    {
+        currentDay = totalDaysOfCurrentYear % daysOfNov;
+        return "Dec";
+    }
+    else
+        return "Unknown month";
+}
+
+int Chapter6Exercises::Exercise6_5() {
+
+    /* (** difficulty)
+     *
+     * An emirp (prime spelled backward) is a prime number whose reversal is also a prime. For example, 79 is a prime
+     * and 97 is a prime, therefore 79 and 97 and emirps. Write a program that displays the first 80 emirps. Display
+     * 8 numbers per line and align the numbers properly, as follows:
+     *
+     *                      2   3   5   7   11  13  17  31
+     *                      37  71  73  79  97  101 107 113
+     */
+
+    E6_5_findEmirp();
+
+    return 0;
+}
+void Chapter6Exercises::E6_5_findEmirp() {
+    // Find a set number of emirps - numbers who are prime, and when their digits are reversed, the reversed number is
+    // also prime
+
+    const int EMIRPS_PER_LINE = 8, TOTAL_EMIRPS_TO_FIND = 80;
+
+    int totalEmirpsFound = 0;
+    int numberToTest = 2; // The first prime number
+
+    cout << left;
+
+    while (totalEmirpsFound < TOTAL_EMIRPS_TO_FIND)
+    {
+
+        if (E6_5_isPrime(numberToTest))
+        {
+            // if function outputs true then the given 'number' is prime
+            if (E6_5_isEmirp(numberToTest))
+            {
+                if ((totalEmirpsFound + 1) % EMIRPS_PER_LINE == 0)
+                    cout << setw(4) << numberToTest << '\n';
+                else
+                    cout << setw(4) << numberToTest << '\t';
+                totalEmirpsFound++;
+            }
+        }
+        numberToTest++; // Move to next number and test while-continuation-condition again
+    }
+}
+bool Chapter6Exercises::E6_5_isPrime(int number) {
+    // Check whether number is prime (exact copy of Listing 5.5)
+
+    for (int divisor = 2; divisor <= number / 2; divisor++)
+    {
+        if (number % divisor == 0)
+        {
+            // If true, number is not prime
+            return false; // Number is not a prime
+        }
+    }
+    return true; // Number is prime
+}
+bool Chapter6Exercises::E6_5_isEmirp(int number) {
+    // Reverses 'number' using code from Exercise 5.4
+
+    int reversedNumber = 0;
+    do {
+        // Moves the 'writing position' along by one column (no effect when reversedNumber = 0), and then adds the last
+        // digit of 'number' to 'reversedNumber'.
+        reversedNumber = reversedNumber * 10 + number % 10;
+        number /= 10; // Removes the added digit
+
+    } while (number > 0);
+
+    if (E6_5_isPrime(reversedNumber))
+        return true;
+    else
+        return false;
 }
